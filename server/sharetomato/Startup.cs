27e44df1374db07a7ae00c477b999b92e687018f
kuluna <mailkuluna@gmail.com>
2017-07-23 10:27:03 +0900

    Redirect https

--- server/sharetomato/Startup.cs
+++ server/sharetomato/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Sharetomato
 {
@@ -29,6 +31,14 @@ namespace Sharetomato
         {
             // Add framework services.
             services.AddMvc();
+
+            if (Configuration.GetValue<bool>("App:ForceHttps"))
+            {
+                services.Configure<MvcOptions>(options =>
+                {
+                    options.Filters.Add(new RequireHttpsAttribute());
+                });
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +46,12 @@ namespace Sharetomato
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            // Force HTTPS
+            if (Configuration.GetValue<bool>("App:ForceHttps"))
+            {
+                app.UseRewriter(new RewriteOptions().AddRedirectToHttps());
+            }
 
             app.UseMvc();
         }
